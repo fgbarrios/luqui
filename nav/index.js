9bc055e8a43750fd27dcc479e5cefd69e2f07576
Fernando Barrios <fernando@nika.agency>
2024-12-31 11:49:52 -0300

    js

---
+++ nav/index.js
@@ -0,0 +1,16 @@
+
+//correr este codigo apenas termine de cargar la pagina
+window.addEventListener("load", () => {
+
+    let elem = document.querySelector('.slider');
+    let flkty = new Flickity( elem, {
+      // options
+      cellAlign: 'left',
+      contain: true,
+      imagesLoaded: true,
+      autoPlay: 1500,
+      wrapAround: true
+    });
+
+});
+
